8d2dd85620f75b7e4937cb9470d8d9b9e9a7772b
Wilfredo Ortez <165108850+WilfredoOrtez@users.noreply.github.com>
2025-12-02 20:08:15 -0600

    ejercicio de mi compañero

---
+++ Program1-semi-2025-Parcial-ejercicio1/Program1-semi-2025-Parcial-ejercicio1/miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace miPrimerProyectoCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblPago = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.Location = new System.Drawing.Point(14, 65);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(74, 24);
+            this.lblMonto.TabIndex = 0;
+            this.lblMonto.Text = "Monto:";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(127, 65);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(143, 20);
+            this.txtMonto.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(353, 65);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(155, 65);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblPago
+            // 
+            this.lblPago.AutoSize = true;
+            this.lblPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPago.Location = new System.Drawing.Point(14, 157);
+            this.lblPago.Name = "lblPago";
+            this.lblPago.Size = new System.Drawing.Size(213, 24);
+            this.lblPago.TabIndex = 3;
+            this.lblPago.Text = "Su valor a pagar es: ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 290);
+            this.Controls.Add(this.lblPago);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblMonto);
+            this.Name = "Form1";
+            this.Text = "IMPUESTO A LAS ACTIVIDADES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblPago;
+    }
+}
+
---
+++ Program1-semi-2025-Parcial-ejercicio1/Program1-semi-2025-Parcial-ejercicio1/miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miPrimerProyectoCsharp
+{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent(); 
+        }
+        private void Form1_Load(object sender, EventArgs e){
+           
+        }
+
+
+        // Matriz con los valores de conversion de uno el inical a los demas
+        double[][] tabla = new double[][]{
+            new double []{0.01 ,500, 1.5,0 }, //1
+            new double []{500.01, 1000, 1.5, 3 }, //2 
+            new double []{1000.01, 2000,3 ,3 }, //3
+            new double []{2000.01, 3000, 6, 3 }, // 4
+            new double []{3000.01, 6000, 9,2 },  //5
+            new double []{8000.01, 18000, 15, 2},  // 6
+            new double []{18000.01, 30000, 39, 2},  // 7
+            new double []{30000.01, 60000, 63, 1},  // 8
+            new double []{60000.01, 100000, 93, 0.8}, // 9
+            new double []{100000.01, 200000, 125, 0.7},  //10
+            new double []{200000.01, 300000, 195, 0.6},  // 11
+            new double []{300000.01, 400000, 255, 0.45},  // 12
+            new double []{400000.01, 500000, 300, 0.4},  //13
+            new double []{500000.01, 1000000, 340, 0.30},  //14
+            new double []{1000000.01, 99999999, 490, 0.18},  // 15
+
+        };
+
+        private double calculo(double monto)
+        {
+            double resultado = 0;
+            // recorre la tabla para encontrar el rango en el que se encuentra el monto
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                // si el monto se encuentra en el rango esta listo para el calculo
+                if (monto >= tabla[i][0] & monto <= tabla[i][1])
+                {
+                    // 1875.79 – 1000.01 = 875.78 / 1000 * 3 + 3 = $5.63
+                    // 20000 – 18000.01 = 1999.99 / 1000 * 2 + 39 = 42.9999 = $43
+                    resultado = monto - tabla[i][0];
+                    resultado = resultado / 1000 * tabla[i][3] + tabla[i][2];
+                }
+            }
+            return resultado;
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double monto = 0;
+            double resultado = 0;
+
+            // si no se ingresa un monto valido
+            if (txtMonto.Text == "")
+            {
+                MessageBox.Show("Ingrese un monto valido");
+            }
+            else
+            {
+                monto = double.Parse(txtMonto.Text);
+
+                resultado = calculo(monto);
+                resultado = Math.Round(resultado, 2);
+                
+
+                lblPago.Text = "Su valor a pagar es: $" + resultado;
+            }
+
+        }
+    }
+}
